bf94149f6dca54ed3b79bd763c36beb165dc38d6
slee3593 <slee3593@gatech.edu>
2023-11-30 16:37:38 +0900

    commit: practice 1-3

--- lib/day3/practice1.dart
+++ lib/day3/practice1.dart
@@ -1,3 +1,14 @@
 class Cleric {
+  int hp = 50;
+  late String name;
+  final int maxHP = 50;
+  final int maxMP = 10;
+  int mp = 10;
+
+  void selfAid() {
+    mp -= 5;
+    hp = maxHP;
+  }
+
 
 }
