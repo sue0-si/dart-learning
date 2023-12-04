87e663dd42cf62a69dee2153c2e153279169ce47
slee3593 <slee3593@gatech.edu>
2023-12-04 15:01:03 +0900

    add: 연습문제 2-2

--- lib/day6/practice2.dart
+++ lib/day6/practice2.dart
@@ -3,6 +3,23 @@
 // 2) 10명 학생의 시험 점수: List
 // 3) 대한민국의 도시별 인구수 (순서 상관 없음): Map
 
+class Person {
+  String name;
+
+  Person({
+    required this.name,
+  });
+}
+
 void main() {
+  Person person1 = Person(name: '홍길동');
+  Person person2 = Person(name: '한석봉');
+
+  List<Person> personList = [person1, person2];
+  final iterator = personList.iterator;
+  while(iterator.moveNext()) {
+    print(iterator.current.name);
+  }
+
 
 }
