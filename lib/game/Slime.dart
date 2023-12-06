b2c9c7a04af7ad1db1d3f9eac9cb3ba1fe4e056d
slee3593 <slee3593@gatech.edu>
2023-12-06 09:46:54 +0900

    add: class codes

---
+++ lib/game/Slime.dart
@@ -0,0 +1,20 @@
+class Slime {
+  int hp;
+  final int level = 10;
+
+  Slime(this.hp);
+
+  void attack() {
+    final person = _Person();
+  }
+}
+
+void main() {
+  Slime slime1 = Slime(100); // 1
+  Slime slime2 = Slime(100); // 2
+
+  // true: O, false: X
+  print(slime1 == slime2);
+}
+
+class _Person {}
