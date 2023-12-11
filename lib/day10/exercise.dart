dadb53d82ed0689aeb30c73722c9320b70ea806b
slee3593 <slee3593@gatech.edu>
2023-12-11 10:03:40 +0900

    revised: 변수 캡슐화

--- lib/day10/exercise.dart
+++ lib/day10/exercise.dart
@@ -1,46 +1,46 @@
 class StrongBox<E> {
   // 금고 클래스에 담는 인스턴스의 타입은 미정
-  E? _money;
+  E? _data;
 
   // 열쇠의 종류를 나타내는 필드 변수
-  KeyType keyType;
+  final KeyType _keyType;
 
   // 열쇠의 종류를 받는 생성자
   StrongBox({
-    required this.keyType,
-  });
+    required KeyType keyType,
+  }) : _keyType = keyType ;
 
   // 메서드로 인스턴스를 저장
-  void put(E money) {
-    _money = money;
+  void put(E data) {
+    _data = data;
   }
 
-  int count = 0;
+  int _count = 0;
 
   // 금고에 저장된 인스턴스를 얻음
   // 금고에서 get() 메서드를 호출할 때 마다 사용횟수를 카운트하고
   // 각 열쇠의 사용횟수에 도달하기 전에는 null을 리턴
   E? get() {
-    count++;
-    switch (keyType) {
+    _count++;
+    switch (_keyType) {
       case KeyType.padlock:
-        if (count < 1024) {
+        if (_count < 1024) {
           return null;
         }
       case KeyType.button:
-        if (count < 10000) {
+        if (_count < 10000) {
           return null;
         }
       case KeyType.dial:
-        if (count < 30000) {
+        if (_count < 30000) {
           return null;
         }
       case KeyType.finger:
-        if (count < 1000000) {
+        if (_count < 1000000) {
           return null;
         }
     }
-    return _money;
+    return _data;
   }
 
 }
@@ -61,7 +61,8 @@ enum KeyType {
 void main() {
   StrongBox box = StrongBox(keyType: KeyType.padlock);
   box.put("1000억");
-  print(box.get());
-  print(box.get());
+  for (int i = 1; i < 1025; i++) {
+      print('$i번째 해제 시도: ${box.get()}');
+  }
 }
 
