4e8ef7996f35a10239231d9780a271bcd9609340
slee3593 <slee3593@gatech.edu>
2023-12-11 17:29:29 +0900

    add: comment

--- lib/day11/employee.dart
+++ lib/day11/employee.dart
@@ -1,3 +1,6 @@
+// 총무부 리더 ‘홍길동(41세)’의 인스턴스를 생성하고 직렬화하여
+// company.txt 파일에 쓰는 프로그램을 작성하시오.
+
 import 'dart:convert';
 import 'dart:io';
 
@@ -10,6 +13,7 @@ class Employee {
     required this.age,
   });
 
+  // 직렬화해주는 메소드
   Map<String, dynamic> toJson() => {
     'name' : name,
     'age' : age
@@ -28,9 +32,10 @@ class Department {
 }
 
 void main() {
+  // 총무부 리더 ‘홍길동(41세)’의 인스턴스
   Employee adminLeader = Employee(name: "홍길동", age: 41);
 
-  print(adminLeader.toJson());
+  // company.txt 파일 생성 & 파일에 json 데이터 넣기
   final companyFile = File("company.txt");
   companyFile.writeAsStringSync(jsonEncode(adminLeader.toJson()));
 }
