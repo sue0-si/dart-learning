0d5038f12f75a2a40bd5d1db354b6d2f724e0d6e
slee3593 <slee3593@gatech.edu>
2023-12-11 10:36:22 +0900

    revised: edge case handling

--- lib/day10/exercise2.dart
+++ lib/day10/exercise2.dart
@@ -1,9 +1,13 @@
 class Word {
-  String word = '';
+  String _word = '';
 
   // i 번째 글자가 모음인지 알려주는 isVowel() 함수
   bool isVowel(int i) {
-    String letter = word.substring(i, i + 1);
+    if (i < 0 || i >= _word.length) {
+      return false;
+    }
+
+    String letter = _word.substring(i, i + 1);
     List<String> vowels = ['a', 'e', 'i', 'o', 'u'];
     List<String> vowelUpper = ['A', 'E', 'I', 'O', 'U'];
 
@@ -21,13 +25,12 @@ class Word {
   }
 
   Word({
-    required this.word,
-  });
+    required String word,
+  }): _word = word;
 }
 
 void main() {
   Word word = Word(word: 'applE');
   print(word.isVowel(4));
   print(word.isConsonant(4));
-
 }
