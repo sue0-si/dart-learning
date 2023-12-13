8e57c2681344abb92ae12e7fe4be8d355503e96b
slee3593 <slee3593@gatech.edu>
2023-12-13 16:57:50 +0900

    add: 비동기 연습1-1

--- lib/day13/file.dart
+++ lib/day13/file.dart
@@ -1,19 +1,17 @@
 import 'dart:io';
 
-void main() async {
+Future<void> main() async {
   File file = File('lib/day13/sample.csv');
-  String text = file.readAsStringSync();
-  print(text);
-
-  final result = await replace(text);
-  print(result);
-
+  await replace(file).catchError((err) => print("에러 발생: $err"));
 }
 
-Future<String> replace(String text) async {
+Future<void> replace(File file) async {
+  String text = file.readAsStringSync();
   String result = text;
   if (text.contains("한석봉")) {
     result = text.replaceAll("한석봉", "김석봉");
   }
-  return result;
+  File copied = File('lib/day13/sample_copy.csv');
+  copied.writeAsStringSync(result);
 }
+
