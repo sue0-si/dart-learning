be850eefd67c91b1798bfecdaf17ddbef3b63e6c
slee3593 <slee3593@gatech.edu>
2023-12-13 14:56:29 +0900

    revised: 다형성 연습1

--- lib/day12/debug1.dart
+++ lib/day12/debug1.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-final String json = {
+final String json = '''{
  "collectionChartDataList": [
    {
      "collectionName": "collectionName",
@@ -20,68 +20,141 @@ final String json = {
      ]
    }
  ]
-} as String;
+}''';
 
+class CollectionChartData {
+  List<ChartData> collectionChartDataList;
 
+  CollectionChartData({required this.collectionChartDataList});
 
-class Test {
-  String cvtDatetime;
-  double price;
+  factory CollectionChartData.fromJson(Map<String, dynamic> json) {
+    List<dynamic> dataList = json['collectionChartDataList'];
+    List<ChartData> chartDataList = dataList
+        .map((data) => ChartData.fromJson(data as Map<String, dynamic>))
+        .toList();
 
-
-  Test({
-    required this.cvtDatetime,
-    required this.price,
-  });
-
-  Map<String, dynamic> toJson() {
-    return {
-      'cvtDatetime': cvtDatetime,
-      'price': price,
-    };
-  }
-
-  factory Test.fromJson(Map<String, dynamic> map) {
-    return Test(
-      cvtDatetime: map['cvtDatetime'],
-      price: map['price'],
-    );
+    return CollectionChartData(collectionChartDataList: chartDataList);
   }
 }
 
-class Collection {
+class ChartData {
   String collectionName;
-  double collectionSalePrice;
+  List<CollectionSalePrice>? collectionSalePrice;
 
-  Collection({
-    required this.collectionName,
-    required this.collectionSalePrice,
-  });
+  ChartData({required this.collectionName, this.collectionSalePrice});
 
-  Map<String, dynamic> toJson() {
-    return {
-      'collectionName': collectionName,
-      'collectionPrice': collectionSalePrice,
-    };
-  }
+  factory ChartData.fromJson(Map<String, dynamic> json) {
+    List<dynamic>? salePriceList = json['collectionSalePrice'];
 
-  factory Collection.fromJson(Map<String, dynamic> map) {
-    return Collection(
-      collectionName: map['collectionName'] as String,
-      collectionSalePrice: map['collectionPrice'] as double,
+    return ChartData(
+      collectionName: json['collectionName'],
+      collectionSalePrice: salePriceList != null
+          ? salePriceList
+          .map(
+            (salePrice) =>
+            CollectionSalePrice.fromJson(salePrice as Map<String, dynamic>),
+      )
+          .toList()
+          : null,
     );
   }
-
 }
 
+class CollectionSalePrice {
+  double price;
+  String cvtDatetime;
+
+  CollectionSalePrice({required this.price, required this.cvtDatetime});
+
+  factory CollectionSalePrice.fromJson(Map<String, dynamic> json) {
+    return CollectionSalePrice(
+      price: json['price'].toDouble(),
+      cvtDatetime: json['cvtDatetime'],
+    );
+  }
+}
 
 void main() {
-  // 단순히 fromJson을 쓰면 collectionName이 정의되어 있지 않아 예외 발생
-  // Collection c1 = Collection.fromJson(jsonDecode(json));
-  // Test c2 = Test.fromJson(jsonDecode(json));
-  // print(c2);
+  // Example usage
+  final jsonString = '''
+  {
+    "collectionChartDataList": [
+      {
+        "collectionName": "collectionName",
+        "collectionSalePrice": null
+      },
+      {
+        "collectionName": "collectionName",
+        "collectionSalePrice": [
+          {
+            "price": 59.75,
+            "cvtDatetime": "2023-03-26T08:08:35"
+          },
+          {
+            "price": 60.00,
+            "cvtDatetime": "2023-03-26T08:08:45"
+          }
+        ]
+      }
+    ]
+  }
+  ''';
 
+  Map<String, dynamic> jsonData = jsonDecode(jsonString);
+  CollectionChartData collectionChartData = CollectionChartData.fromJson(jsonData);
 
-
-
+  print(collectionChartData.collectionChartDataList);
 }
+
+
+
+
+// class Test {
+//   String cvtDatetime;
+//   double price;
+//
+//
+//   Test({
+//     required this.cvtDatetime,
+//     required this.price,
+//   });
+//
+//   Map<String, dynamic> toJson() {
+//     return {
+//       'cvtDatetime': cvtDatetime,
+//       'price': price,
+//     };
+//   }
+//
+//   factory Test.fromJson(Map<String, dynamic> map) {
+//     return Test(
+//       cvtDatetime: map['cvtDatetime'],
+//       price: map['price'],
+//     );
+//   }
+// }
+//
+// class Collection {
+//   String collectionName;
+//   double collectionSalePrice;
+//
+//   Collection({
+//     required this.collectionName,
+//     required this.collectionSalePrice,
+//   });
+//
+//   Map<String, dynamic> toJson() {
+//     return {
+//       'collectionName': collectionName,
+//       'collectionPrice': collectionSalePrice,
+//     };
+//   }
+//
+//   factory Collection.fromJson(Map<String, dynamic> map) {
+//     return Collection(
+//       collectionName: map['collectionName'] as String,
+//       collectionSalePrice: map['collectionPrice'] as double,
+//     );
+//   }
+//
+// }
