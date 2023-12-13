034f8fe76e8e050cc3894de513c66a9a8de48a90
slee3593 <slee3593@gatech.edu>
2023-12-13 17:04:03 +0900

    add: comment

--- lib/day13/file.dart
+++ lib/day13/file.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 
 Future<void> main() async {
   File file = File('lib/day13/sample.csv');
+
+  // 예외 처리
   await replace(file).catchError((err) => print("에러 발생: $err"));
 }
 
+// 한석봉을 김석봉으로 수정하는 함수
 Future<void> replace(File file) async {
   String text = file.readAsStringSync();
   String result = text;
