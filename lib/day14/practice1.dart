7b97071fa67af6319be2d160a74341f27f7fcc56
slee3593 <slee3593@gatech.edu>
2023-12-14 16:01:23 +0900

    add: 네트워크 연습1

---
+++ lib/day14/practice1.dart
@@ -0,0 +1,145 @@
+// 연습문제1: 영화정보를 가져와서 모델에 담고 json 형태로 출력하기
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  final movieInfo = await getMovie();
+  print(jsonEncode(movieInfo.toJson()));
+}
+
+Future<Movie> getMovie() async {
+  // 요청
+  final response = await http.get(Uri.parse('https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1'));
+  return Movie.fromJson(jsonDecode(response.body));
+}
+
+class Movie {
+  Dates? dates;
+  int? page;
+  List<Results>? results;
+  int? totalPages;
+  int? totalResults;
+
+  Movie(
+      {this.dates,
+        this.page,
+        this.results,
+        this.totalPages,
+        this.totalResults});
+
+  Movie.fromJson(Map<String, dynamic> json) {
+    dates = json['dates'] != null ? Dates.fromJson(json['dates']) : null;
+    page = json['page'];
+    if (json['results'] != null) {
+      results = <Results>[];
+      json['results'].forEach((v) {
+        results!.add(Results.fromJson(v));
+      });
+    }
+    totalPages = json['total_pages'];
+    totalResults = json['total_results'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (dates != null) {
+      data['dates'] = dates!.toJson();
+    }
+    data['page'] = page;
+    if (results != null) {
+      data['results'] = results!.map((v) => v.toJson()).toList();
+    }
+    data['total_pages'] = totalPages;
+    data['total_results'] = totalResults;
+    return data;
+  }
+}
+
+class Dates {
+  String? maximum;
+  String? minimum;
+
+  Dates({this.maximum, this.minimum});
+
+  Dates.fromJson(Map<String, dynamic> json) {
+    maximum = json['maximum'];
+    minimum = json['minimum'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['maximum'] = maximum;
+    data['minimum'] = minimum;
+    return data;
+  }
+}
+
+class Results {
+  bool? adult;
+  String? backdropPath;
+  List<int>? genreIds;
+  int? id;
+  String? originalLanguage;
+  String? originalTitle;
+  String? overview;
+  double? popularity;
+  String? posterPath;
+  String? releaseDate;
+  String? title;
+  bool? video;
+  double? voteAverage;
+  int? voteCount;
+
+  Results(
+      {this.adult,
+        this.backdropPath,
+        this.genreIds,
+        this.id,
+        this.originalLanguage,
+        this.originalTitle,
+        this.overview,
+        this.popularity,
+        this.posterPath,
+        this.releaseDate,
+        this.title,
+        this.video,
+        this.voteAverage,
+        this.voteCount});
+
+  Results.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    genreIds != null ? json['genre_ids'] : 0;
+    id = json['id'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    popularity = json['popularity'];
+    posterPath = json['poster_path'];
+    releaseDate = json['release_date'];
+    title = json['title'];
+    video = json['video'];
+    voteAverage = json['vote_average'];
+    voteCount = json['vote_count'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['adult'] = adult;
+    data['backdrop_path'] = backdropPath;
+    data['genre_ids'] = genreIds;
+    data['id'] = id;
+    data['original_language'] = originalLanguage;
+    data['original_title'] = originalTitle;
+    data['overview'] = overview;
+    data['popularity'] = popularity;
+    data['poster_path'] = posterPath;
+    data['release_date'] = releaseDate;
+    data['title'] = title;
+    data['video'] = video;
+    data['vote_average'] = voteAverage;
+    data['vote_count'] = voteCount;
+    return data;
+  }
+}
