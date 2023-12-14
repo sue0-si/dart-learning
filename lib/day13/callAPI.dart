2eaf0fe1a21f7f21d46d476a7de9ef28e9118d30
slee3593 <slee3593@gatech.edu>
2023-12-14 10:16:51 +0900

    revised: 클래스로 json 처리

--- lib/day13/callAPI.dart
+++ lib/day13/callAPI.dart
@@ -12,7 +12,39 @@ Future<String> getMovieInfo() async {
 
 Future<void> main() async {
   // json을 map으로 변환
-  Map<String, dynamic> jsonData = jsonDecode(await getMovieInfo());
+  // Map<String, dynamic> jsonData = jsonDecode(await getMovieInfo());
   // director의 value 출력
-  print(jsonData['director']);
+  // print(jsonData['director']);
+
+  // 클래스를 만들어 json <-> map 자유롭게 변환
+  final String movieInfo = await getMovieInfo();
+  print(MovieInfo.fromJson(jsonDecode(movieInfo)).director);
+}
+
+class MovieInfo {
+  String title;
+  String director;
+  int year;
+
+  MovieInfo({
+    required this.title,
+    required this.director,
+    required this.year,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'director': director,
+      'year': year,
+    };
+  }
+
+  factory MovieInfo.fromJson(Map<String, dynamic> map) {
+    return MovieInfo(
+      title: map['title'] as String,
+      director: map['director'] as String,
+      year: map['year'] as int,
+    );
+  }
 }
